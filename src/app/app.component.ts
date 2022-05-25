3463d4b4b20ba0ab70776c5ded52c00faf48e37d
Roman <43218195+ryamchik@users.noreply.github.com>
2022-05-25 17:10:18 +0300

    Добавлена итоговая сумма покупок

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
+
+class Item{
+  purchase: string;
+  done: boolean;
+  price: number;
+  
+  constructor(purchase: string, price: number) {
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-Angular-app';
+  text: string = "";
+  price: number = 0;
+  totalSum: number = 0;
+
+  items: Item[] = 
+  [
+    { purchase: "Хлеб", done: false, price: 15.9},
+    { purchase: "Масло", done: false, price: 15.9},
+    { purchase: "Картофель", done: false, price: 15.9},
+    { purchase: "Сыр", done: false, price: 15.9},
+  ];
+  addItem(text: string, price: number): void {
+    if(text==null || text.trim()=="" || price==null)
+      return;
+    this.items.push(new Item(text, price));
+    this.totalSum = this.sumPrice();
+  }
+  sumPrice():number {
+    return this.items.reduce((accumulator, item) => {
+      return accumulator + item.price;
+    }, 0);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
